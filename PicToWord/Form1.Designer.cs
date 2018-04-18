893d1c78eb906d77a9469425f4c58321a1f5553c
xinshuaitong <xinstong@gmail.com>
2018-04-18 11:59:12 +0800

    ok

---
+++ PicToWord/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace PicToWord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.progressBarHandle = new System.Windows.Forms.ProgressBar();
+            this.buttonStartHandle = new System.Windows.Forms.Button();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(806, 8);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 0;
+            this.buttonBrowse.Text = "浏览";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // progressBarHandle
+            // 
+            this.progressBarHandle.Location = new System.Drawing.Point(13, 36);
+            this.progressBarHandle.Name = "progressBarHandle";
+            this.progressBarHandle.Size = new System.Drawing.Size(787, 23);
+            this.progressBarHandle.TabIndex = 2;
+            // 
+            // buttonStartHandle
+            // 
+            this.buttonStartHandle.Enabled = false;
+            this.buttonStartHandle.Location = new System.Drawing.Point(806, 35);
+            this.buttonStartHandle.Name = "buttonStartHandle";
+            this.buttonStartHandle.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartHandle.TabIndex = 3;
+            this.buttonStartHandle.Text = "开始处理";
+            this.buttonStartHandle.UseVisualStyleBackColor = true;
+            this.buttonStartHandle.Click += new System.EventHandler(this.buttonStartHandle_Click);
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.Location = new System.Drawing.Point(13, 66);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(868, 459);
+            this.listBoxResult.TabIndex = 4;
+
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(12, 11);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(788, 20);
+            this.textBoxPath.TabIndex = 5;
+            this.textBoxPath.TextChanged += new System.EventHandler(this.textBoxPath_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 540);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.buttonStartHandle);
+            this.Controls.Add(this.progressBarHandle);
+            this.Controls.Add(this.buttonBrowse);
+            this.Name = "Form1";
+            this.Text = "批量处理工具-插入图片到word文档最后";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.ProgressBar progressBarHandle;
+        private System.Windows.Forms.Button buttonStartHandle;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.TextBox textBoxPath;
+    }
+}
+
---
+++ PicToWord/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
+
+namespace PicToWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "请选择Word和图片所在文件夹";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    this.buttonStartHandle.Enabled = false;
+                    MessageBox.Show(this, "文件夹路径不能为空", "提示");
+                    return;
+                }
+                this.textBoxPath.Text = dialog.SelectedPath;
+                this.buttonStartHandle.Enabled = true;
+            }
+        }
+
+        delegate void ShowProgressDelegate(int totalStep, int currentStep);
+        delegate void ShowHandleResultDelegate(int totalStep, int currentStep, string wordName, string picName, int result);
+        delegate void RunTaskDelegate(string dirRootPath);
+        private void buttonStartHandle_Click(object sender, EventArgs e)
+        {
+            RunTaskDelegate runTask = new RunTaskDelegate(RunTask);
+
+            runTask.BeginInvoke(this.textBoxPath.Text, null, null);
+        }
+
+        void ShowProgress(int totalStep, int currentStep)
+        {
+            if( this.progressBarHandle.InvokeRequired)
+            {
+                ShowProgressDelegate showProgress = new ShowProgressDelegate(ShowProgress );
+
+                this.BeginInvoke( showProgress, new object[] { totalStep, currentStep } );
+            }
+            else
+            {
+                this.progressBarHandle.Maximum = totalStep;
+                this.progressBarHandle.Value = currentStep;
+            }
+        }
+
+        private string item;
+        void ShowHandleResult(int totalStep, int currentStep, string wordName, string picName, int result)
+        {
+            
+            if (this.listBoxResult.InvokeRequired)
+            {
+                ShowHandleResultDelegate reportHandleResult = new ShowHandleResultDelegate(ShowHandleResult);
+
+                this.BeginInvoke(reportHandleResult, new object[] { totalStep, currentStep, wordName, picName, result });
+            }
+            else
+            {
+                item = "";
+                item += "(";
+                item += currentStep;
+                item += "/";
+                item += totalStep;
+                item += ")";
+                if (result == 0)
+                {
+                    item += "OK.";
+                }
+                else
+                {
+                    item += "ERROR.";
+                }
+                item += "   [";
+                item += wordName;
+                item += "]   [";
+                item += picName;
+                item += "]";
+
+                this.listBoxResult.Items.Add(item);
+            }
+        }
+
+        void RunTask(string dirRootPath)
+        {
+            allFileListWord.Clear();
+            allFileListPic.Clear();
+            GetAllDirList(dirRootPath);
+
+            for (int i = 0; i < allFileListWord.Count; i++)
+            {
+                int iResult = 0;
+                do
+                {
+                    string wordName = allFileListWord[i].ToString();
+                    string[] keyNameArr = wordName.Split('.');
+                    if (keyNameArr.Length == 0)
+                    {
+                        iResult = -1;
+                        break;
+                    }
+                    string keyName = keyNameArr[0];
+                    bool bFinded = false;
+                    string toInsertPicFullName = "";
+                    foreach(object picName in  allFileListPic)
+                    {
+                        toInsertPicFullName = picName.ToString();
+                        if (toInsertPicFullName.Contains(keyName))
+                        {
+                            bFinded = true;
+                            break;
+                        }
+                    }
+                    if (!bFinded)
+                    {
+                        iResult = -1;
+                        break;
+                    }
+
+                    Document document = new Document(allFileListWord[i].ToString());
+                    Section s = document.AddSection();
+                    Paragraph p = s.AddParagraph();
+                    DocPicture Pic = p.AppendPicture(Image.FromFile(toInsertPicFullName));
+                    Pic.Width = 750;
+                    Pic.Height = 468;
+
+                    //Save and Launch
+                    if (keyNameArr[keyNameArr.Length - 1].Equals(".docx"))
+                    {
+                        document.SaveToFile(wordName, FileFormat.Docx);
+                    }
+                    else
+                    {
+                        document.SaveToFile(wordName, FileFormat.Doc);
+                    }
+
+                    //System.Diagnostics.Process.Start(wordName);
+
+
+
+                    ShowProgress(allFileListWord.Count, i + 1);
+                }while(false);
+
+                string wordShortName = "";
+                string picShortName = "";
+                wordShortName = allFileListWord[i].ToString();
+                wordShortName = wordShortName.Substring(dirRootPath.Length, wordShortName.Length - dirRootPath.Length);
+                picShortName = allFileListPic[i].ToString();
+                picShortName = picShortName.Substring(dirRootPath.Length, picShortName.Length - dirRootPath.Length);
+                ShowHandleResult(allFileListWord.Count, i + 1,
+                    wordShortName, picShortName, iResult);
+            }
+        }
+
+        private ArrayList allFileListWord = new ArrayList();
+        private ArrayList allFileListPic = new ArrayList();
+        
+        public void GetAllDirList(string strBaseDir)
+        {
+            DirectoryInfo di = new DirectoryInfo(strBaseDir);
+
+            foreach(DirectoryInfo subDi in di.GetDirectories())
+            {
+                GetAllDirList(subDi.FullName);
+            }
+
+            foreach (FileInfo file in di.GetFiles("*.doc"))
+            {
+                allFileListWord.Add(file.FullName);
+            }
+            foreach (FileInfo file in di.GetFiles("*.docx"))
+            {
+                allFileListWord.Add(file.FullName);
+            }
+
+            foreach (FileInfo file in di.GetFiles("*.jpg"))
+            {
+                allFileListPic.Add(file.FullName);
+            }
+            foreach (FileInfo file in di.GetFiles("*.png"))
+            {
+                allFileListPic.Add(file.FullName);
+            }
+        }
+
+        private void textBoxPath_TextChanged(object sender, EventArgs e)
+        {
+            this.buttonStartHandle.Enabled = Directory.Exists(this.textBoxPath.Text);
+        }
+
+        private void listBoxResult_DrawItem(object sender, DrawItemEventArgs e)
+        {
+
+        }
+    }
+}
